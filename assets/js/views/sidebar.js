cdead07cb5656246e24a7c6208ccbff4fd244c64
Cal Milne <cal@leetpc.com.au>
2014-03-02 03:26:26 +1100

    Sidebar and layout updates

--- assets/js/views/sidebar.js
+++ assets/js/views/sidebar.js
@@ -5,9 +5,25 @@ $(function() {
         id: 'main-sidebar',
         tagName: 'div',
 
+        template: function() {
+            return $( '<div class="sidebar-item">'
+                + '<a class="sidebar-button icon-add" href="#">Add new...</a>'
+                + '</div>'
+                + '<div class="sidebar-item">'
+                + '<a class="sidebar-button icon-lists" href="#/lists">All lists</a>'
+                + '</div>'
+                + '<div class="sidebar-item">'
+                + '<a class="sidebar-button icon-calendar" href="#/calendar">Calendar</a>'
+                + '</div>'
+                + '<div class="sidebar-item">'
+                + '<a class="sidebar-button icon-today" href="#/today">Today</a>'
+                + '</div>'
+            );
+        },
+
         initialize: function() {
 
-            this.$el.append("sidebarring!");
+            this.$el.append(this.template());
 
         },
 
