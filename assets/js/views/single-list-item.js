fc89b40003aca0dbb00d6aac30cf977538d47da9
Cal Milne <cal@leetpc.com.au>
2014-03-03 22:12:56 +1100

    Single List Item View updates

--- assets/js/views/single-list-item.js
+++ assets/js/views/single-list-item.js
@@ -7,12 +7,32 @@ $(function() {
 
         initialize: function() {
             
-            this.$el.append(this.model.get('Summary'));
+            this.els = {};
+
+            this.els.summary = $('<div class="summary"></div>');
+            this.els.checkbox = $('<div class="checkbox"></div>');
+
+            this.els.checkbox.html('<div class="outline"><div class="indicator"></div></div>');
+
+            this.$el
+                .append(this.els.summary)
+                .append(this.els.checkbox);
 
         },
 
         render: function() {
+
+            this.els.summary.html(this.model.get('Summary'));
+
+            if (this.model.get('Completed')) {
+                this.els.checkbox.addClass('checked');
+            }
+            else {
+                this.els.checkbox.removeClass('checked');
+            }
+
             return this;
+
         },
 
     });
