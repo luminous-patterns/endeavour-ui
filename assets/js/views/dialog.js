ebef28b169e68089a9db78d4d265cb3916af1308
Cal Milne <cal@leetpc.com.au>
2014-03-23 14:44:27 +1100

    Dialog view - Added 'textarea' and 'datetime' form element types - Changed DialogAddNewListItem view to extend Dialog view

--- assets/js/views/dialog.js
+++ assets/js/views/dialog.js
@@ -26,7 +26,7 @@ $(function() {
             this.els.title = $('<div class="dialog-title">Add List</div>');
             this.els.form = $('<form></form>');
 
-            this.els.buttonSection = $('<div class="dialog-section button-section"><button class="cancel">Cancel</button><button class="call-to-action">Create</button></div>');
+            this.els.buttonSection = $('<div class="dialog-section button-section"><button type="button" class="cancel">Cancel</button><button type="submit" class="call-to-action">Create</button></div>');
             this.els.submitButton = this.els.buttonSection.find('.call-to-action');
             this.els.cancelButton = this.els.buttonSection.find('.cancel');
 
@@ -40,10 +40,11 @@ $(function() {
             this.$el
                 .append(this.els.title)
                 .append(this.els.form)
-                .append(this.els.buttonSection)
                 .prepend(this.els.errorContainer);
 
             this.createFormEls();
+
+            this.els.form.append(this.els.buttonSection);
 
             console.log('### initialize dialog');
 
@@ -71,6 +72,24 @@ $(function() {
                             + '<label for="' + field.id + '">' + field.label + '</label>'
                             + '<input type="text" id="' + field.id + '" class="full-width" value="' + field.value + '" />'
                             + '</div>');
+                        field.$el = field.containerEl.find('#' + field.id);
+                        break;
+                    case 'textarea':
+                        field.containerEl = $('<div class="dialog-section">'
+                            + '<label for="' + field.id + '">' + field.label + '</label>'
+                            + '<textarea id="' + field.id + '" class="full-width">' + field.value + '</textarea>'
+                            + '</div>');
+                        field.$el = field.containerEl.find('#' + field.id);
+                        break;
+                    case 'datetime':
+                        field.containerEl = $('<div class="dialog-section">'
+                            + '<label for="' + field.id + '-year">Due on</label>'
+                            + '<div class="input date-input"><input type="text" id="' + field.id + '-year" value="YYYY" /><span>-</span><input type="text" id="' + field.id + '-month" value="MM" /><span>-</span><input type="text" id="' + field.id + '-date" value="DD" /></div>'
+                            + ' @ '
+                            + '<div class="input time-input"><input type="text" id="' + field.id + '-hour" value="HH" /><span>:</span><input type="text" id="' + field.id + '-minute" value="MM" /></div>'
+                            + '<input type="hidden" id="' + field.id + '" value="' + field.value + '" />'
+                            + '</div>');
+                        field.containerEl.find('input').on('change', $.proxy(this.onDateTimeInputChange, field));
                         field.$el = field.containerEl.find('#' + field.id);
                         break;
                 }
@@ -131,17 +150,47 @@ $(function() {
             return this;
         },
 
-        onClickSubmit: function() {
+        onClickSubmit: function(ev) {
+            ev.preventDefault();
             return this.submit();
         },
 
-        onClickCancel: function() {
+        onClickCancel: function(ev) {
+            ev.preventDefault();
             return this.closeDialog();
         },
 
         onSubmitForm: function(ev) {
             ev.preventDefault();
             return this.submit();
+        },
+
+        onDateTimeInputChange: function(ev) {
+
+            // Context of a field item
+            var field = this;
+
+            // The changed el
+            var el = $(ev.target);
+            console.log(field,this,el);
+
+            if (el.attr('id') == field.id) return;
+
+            var yearEl = field.$el.find('#' + field.id + '-year');
+            var monthEl = field.$el.find('#' + field.id + '-month');
+            var dateEl = field.$el.find('#' + field.id + '-date');
+
+            var hourEl = field.$el.find('#' + field.id + '-hour');
+            var minuteEl = field.$el.find('#' + field.id + '-minute');
+
+            var yyyy = !yearEl.val() || yearEl.val() == 'YYYY' ? '2014' : yearEl.val();
+            var mo = !monthEl.val() || monthEl.val() == 'MM' ? '01' : monthEl.val();
+            var dd = !dateEl.val() || dateEl.val() == 'DD' ? '01' : dateEl.val();
+            var hh = !hourEl.val() || hourEl.val() == 'HH' ? '00' : hourEl.val();
+            var mi = !minuteEl.val() || minuteEl.val() == 'MM' ? '00' : minuteEl.val();
+
+            field.$el.val(yyyy + '-' + mo + '-' + dd + ' ' + hh + ':' + mi + ':00');
+
         },
 
     });
--- assets/js/views/dialogs/add-new-list-item.js
+++ assets/js/views/dialogs/add-new-list-item.js
@@ -1,111 +1,33 @@
 $(function() {
 
-    window.Endeavour.View.DialogAddNewListItem = Backbone.Marionette.View.extend({
+    window.Endeavour.View.DialogAddNewListItem = Endeavour.View.Dialog.extend({
 
         id: 'add-new-dialog',
-        tagName: 'div',
-        className: 'dialog',
 
-        events: {
-            'click':          'onClick',
+        fields: [{
+            id: 'item-summary',
+            type: 'input',
+            label: 'Summary',
+            value: '',
+            inputKey: 'Summary',
+            pos: 1,
         },
-
-        initialize: function() {
-
-            this.els = {};
-            this.validationErrorMessage = '';
-
-            this.els.title = $('<div class="dialog-title">Add List Item</div>');
-
-            this.els.summarySection = $('<div class="dialog-section"><label for="summary">Summary</label><input type="text" id="summary" class="full-width" /></div>');
-            this.els.summaryInput = this.els.summarySection.find('#summary');
-
-            this.els.detailsSection = $('<div class="dialog-section"><label for="details">Details (e.g. chocolate, with sprinkles)</label><textarea id="details" class="full-width"></textarea></div>');
-            this.els.detailsInput = this.els.detailsSection.find('#details');
-
-            this.els.dueDateSection = $('<div class="dialog-section"><label for="due-hour">Due on</label>'
-                + '<div class="input date-input"><input type="text" id="due-year" value="YYYY" /><span>-</span><input type="text" id="due-month" value="MM" /><span>-</span><input type="text" id="due-date" value="DD" /></div>'
-                + ' @ '
-                + '<div class="input time-input"><input type="text" id="due-hour" value="HH" /><span>:</span><input type="text" id="due-minute" value="MM" /></div>'
-                + '</div>'
-            );
-            this.els.dueDateInput = this.els.dueDateSection.find('#due-date');
-            this.els.dueTimeInput = this.els.dueDateSection.find('#due-time');
-
-            this.els.buttonSection = $('<div class="dialog-section button-section"><button class="cancel">Cancel</button><button class="call-to-action">Create</button></div>');
-            this.els.submitButton = this.els.buttonSection.find('.call-to-action');
-            this.els.cancelButton = this.els.buttonSection.find('.cancel');
-
-            this.els.errorMessage = $('<div class="error-message"></div>');
-            this.els.errorContainer = $('<div class="dialog-section error-section"></div>');
-
-            this.els.errorContainer
-                .append(this.els.errorMessage)
-                .hide();
-
-            this.$el
-                .append(this.els.title)
-                .append(this.els.summarySection)
-                .append(this.els.detailsSection)
-                .append(this.els.dueDateSection)
-                .append(this.els.buttonSection)
-                .prepend(this.els.errorContainer);
-
-            this.els.submitButton.on('click', $.proxy(this.onClickSubmit, this));
-            this.els.cancelButton.on('click', $.proxy(this.onClickCancel, this));
-
-            this.$el.on('keypress', $.proxy(this.onKeyPress, this));
-
-            console.log('### initialize stage view');
-
+        {
+            id: 'item-details',
+            type: 'textarea',
+            label: 'Details (e.g. chocolate, with sprinkles)',
+            value: '',
+            inputKey: 'Details',
+            pos: 2,
         },
-
-        render: function() {
-            console.log('### render stage view');
-            return this;
-        },
-
-        focusField: function() {
-            this.els.summaryInput.focus();
-            return this;
-        },
-
-        getInputs: function() {
-
-            var that = this;
-
-            var inputs = {
-                Summary: that.els.summaryInput.val(),
-                Details: that.els.detailsInput.val(),
-            };
-
-            return inputs;
-
-        },
-
-        validInputs: function() {
-
-            var inputs = this.getInputs();
-
-            if (!inputs.Summary || inputs.Summary.length < 3) {
-                this.validationErrorMessage = 'Invalid Summary';
-                return false;
-            }
-
-            return true;
-
-        },
-
-        showError: function(error) {
-            this.els.errorMessage.html(error);
-            this.els.errorContainer.show();
-            return this;
-        },
-
-        hideError: function() {
-            this.els.errorContainer.hide();
-            return this;
-        },
+        {
+            id: 'item-due',
+            type: 'datetime',
+            label: 'Due on',
+            value: '',
+            inputKey: 'Due',
+            pos: 3,
+        }],
 
         submit: function() {
 
@@ -118,34 +40,6 @@ $(function() {
 
             return this;
 
-        },
-
-        onKeyPress: function(ev) {
-            if (event.which == 13) {
-                return this.submit();
-            }
-        },
-
-        onClick: function(ev) {
-            ev.stopPropagation();
-            return this;
-        },
-
-        onClickSubmit: function() {
-
-            console.log('add new list item submit',this.getInputs());
-
-            return this.submit();
-
-        },
-
-        onClickCancel: function() {
-            return this.closeDialog();
-        },
-
-        closeDialog: function() {
-            this.trigger('close');
-            return this.close();
         },
 
     });
